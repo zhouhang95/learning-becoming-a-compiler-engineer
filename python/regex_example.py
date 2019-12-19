382c6c2a787cf798ecc8dd2424b852b585d3f697
zhouhang95 <765229842@qq.com>
2019-12-19 11:01:45 +0800

    regex examples

---
+++ python/regex_example.py
@@ -0,0 +1,12 @@
+examples = {
+    'a': {
+        True: ['a'],
+        False: ['b', 'ab'],
+    },
+    'a*': None,
+    'a+b': None,
+    'a|b': None,
+    '(a)': None,
+    '(a)*': None,
+    '(a)*|a+b': None,
+}
