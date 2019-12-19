8daca47cadd55e80f344c545f25dc74603b1f53a
zhouhang95 <765229842@qq.com>
2019-12-19 11:29:19 +0800

    transition use list<tuple> rather than dict

--- python/regex.py
+++ python/regex.py
@@ -4,15 +4,15 @@ class FiniteAutomata:
         self.states = []
         self.startstate = None
         self.finalstates = []
-        self.transitions = {}
+        self.transitions = []
     
     def index_offset(self, offset: int):
         self.startstate += offset
         self.states = list(map(lambda x: x+offset, self.states))
         self.finalstates = list(map(lambda x: x+offset, self.finalstates))
-        transitions = {}
-        for k, v in self.transitions.items():
-            transitions[(k[0]+offset, k[1]+offset)] = v
+        transitions = []
+        for node_0, node_1, edge in self.transitions:
+            transitions.append((node_0+offset, node_1+offset, edge))
         self.transitions = transitions
 
     def __str__(self):
@@ -21,8 +21,8 @@ class FiniteAutomata:
         sink += '\nstartstate: ' + str(self.startstate)
         sink += '\nfinalstates: ' + ','.join(map(str, self.finalstates))
         sink += '\ntransitions:'
-        for k, v in self.transitions.items():
-            sink += '\n\t{} -> {}: {}'.format(k[0], k[1], v)
+        for node_0, node_1, edge in self.transitions:
+            sink += '\n\t{} -> {}: {}'.format(node_0, node_1, edge)
 
         return sink
 
@@ -47,7 +47,7 @@ class FiniteAutomata:
         fa.startstate = 0
         fa.states.extend([0, 1])
         fa.finalstates.append(1)
-        fa.transitions[(0, 1)] = char
+        fa.transitions.append((0, 1, char))
         return fa
 
     @staticmethod
@@ -60,9 +60,8 @@ class FiniteAutomata:
         fa.startstate = 0
         fa.states = a.states + b.states
         fa.finalstates = b.finalstates
-        fa.transitions.update(a.transitions)
-        fa.transitions.update(b.transitions)
-        fa.transitions[(a.finalstates[0], b.startstate)] = 'epsilon'
+        fa.transitions = a.transitions + b.transitions
+        fa.transitions.append((a.finalstates[0], b.startstate, None))
 
         return fa
 
@@ -80,14 +79,13 @@ class FiniteAutomata:
         finalstate = 1 + a_len + b_len
         fa.states = a.states + b.states + [0, finalstate]
         fa.finalstates = [finalstate]
-        fa.transitions = {
-            (0, a.startstate): 'epsilon',
-            (0, b.startstate): 'epsilon',
-            (a.finalstates[0], finalstate): 'epsilon',
-            (b.finalstates[0], finalstate): 'epsilon',
-        }
-        fa.transitions.update(a.transitions)
-        fa.transitions.update(b.transitions)
+        fa.transitions = [
+            (0, a.startstate, None),
+            (0, b.startstate, None),
+            (a.finalstates[0], finalstate, None),
+            (b.finalstates[0], finalstate, None),
+        ]
+        fa.transitions += a.transitions + b.transitions
         
         return fa
     
@@ -101,39 +99,27 @@ class FiniteAutomata:
         finalstate = 1 + a_len
         fa.states = a.states + [0, finalstate]
         fa.finalstates = [finalstate]
-        fa.transitions = {
-            (0, a.startstate): 'epsilon',
-            (a.finalstates[0], finalstate): 'epsilon',
-            (a.finalstates[0], a.startstate): 'epsilon',
-            (0, finalstate): 'epsilon',
-        }
-        fa.transitions.update(a.transitions)
+        fa.transitions = [
+            (0, a.startstate, None),
+            (a.finalstates[0], finalstate, None),
+            (a.finalstates[0], a.startstate, None),
+            (0, finalstate, None),
+        ]
+        fa.transitions += a.transitions
 
         return fa
 
-    @staticmethod
-    def positive_closure(a):
-        a_len = len(a.states)
-        a.index_offset(1)
-        fa = FiniteAutomata()
-        fa.fa_type = 'NFA'
-        fa.startstate = 0
-        finalstate = 1 + a_len
-        fa.states = a.states + [0, finalstate]
-        fa.finalstates = [finalstate]
-        fa.transitions = {
-            (0, a.startstate): 'epsilon',
-            (a.finalstates[0], finalstate): 'epsilon',
-            (a.finalstates[0], a.startstate): 'epsilon',
-        }
-        fa.transitions.update(a.transitions)
-        
-        return fa
 
 def regex2nfa(regex: str):
     pass
 
 def main():
+    a = FiniteAutomata.base('a')
+    b = FiniteAutomata.base('b')
+    print(FiniteAutomata.concatention(a, b))
+    a = FiniteAutomata.base('a')
+    b = FiniteAutomata.base('b')
+    print(FiniteAutomata.alternation(a, b))
     a = FiniteAutomata.base('a')
     b = FiniteAutomata.base('b')
     print(FiniteAutomata.kleene_closure(a))
